4357ffa1dc0307942b52d28fd0fcb687ae3df20e
Arafat Hasan Akib <websited98@gmail.com>
2026-04-19 20:21:19 +0600

    Add files via upload

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,6 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Arafat", "Akib"]
+
+print(friends[0])
+friends[0] = "Grapes"
+
+print(friends[0])
---
+++ Chapter 4/02_list_methods.py
@@ -0,0 +1,12 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Arafat", "Hasan"]
+print(friends)
+friends.append("Akib")
+print(friends)
+
+li =  [1,  66, 33, 97, 23, 565]
+# li.sort()
+# li.reverse()
+# li.insert(3, 4564) # Insert 4354 such that  its index in the list is 3
+# print(li.pop(3))
+# li.remove(33)
+print(li)
