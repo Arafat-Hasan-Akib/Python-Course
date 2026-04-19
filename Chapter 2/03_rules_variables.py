85e3d6354146c196237652b3347cbbb71b731bef
Arafat Hasan Akib <websited98@gmail.com>
2026-04-19 20:15:49 +0600

    Add files via upload

---
+++ Chapter 2/03_rules_variables.py
@@ -0,0 +1,12 @@
+a = 23
+
+aaa = 432
+
+akib = 34
+
+sameer = 45
+
+_sameer = 34
+
+# @sameer = 56 # invalid
+
---
+++ Chapter 2/05_type.py
@@ -0,0 +1,7 @@
+a = "31.33" 
+b = float(a) # a but the type should be float
+t = type(a) # class <int>
+print(t)
+
+x = type(b)
+print(x)
