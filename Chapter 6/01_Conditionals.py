17b0d849dacbb178e94018281192cfa94c81f317
Arafat Hasan Akib <websited98@gmail.com>
2026-04-19 20:26:00 +0600

    Add files via upload

---
+++ Chapter 6/01_Conditionals.py
@@ -0,0 +1,15 @@
+a = int(input("Enter Your Age: "))
+
+# If else statement
+
+if(a>=18):
+    print("Your are above the age of consent")
+    print("Good for you")
+
+
+else:
+    print("You are below the age of consent")
+
+
+
+print("End of program")
