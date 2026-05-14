1a76732f87d14510e4fcc60064d6f18c404fa214
Arafat Hasan Akib <websited98@gmail.com>
2026-05-14 20:35:42 +0600

    Add files via upload

---
+++ Chapter 8 ps/01_Problem1.py
@@ -0,0 +1,14 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+
+def greatest(a, b, c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b    
+    elif(c>a and c>b):
+        return c
+    
+
+print("The greatest number is: ", greatest(a, b, c))
