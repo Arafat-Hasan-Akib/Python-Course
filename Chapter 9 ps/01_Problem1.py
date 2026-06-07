7349d8159700a65937b3191472538fdc84d31759
Arafat Hasan Akib <websited98@gmail.com>
2026-06-07 20:22:44 +0600

    Add files via upload

---
+++ Chapter 9 ps/01_Problem1.py
@@ -0,0 +1,10 @@
+f = open("poem.txt", "r")
+
+content = f.read()
+if("twinkle" in content):
+    print("Twinkle is present in the poem")
+
+else:
+    print("Twinkle is not present in the poem")    
+
+f.close()
